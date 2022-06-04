a293dd48599b3aff8fbd6f0879eee3993214be42
Muh Ichlasul Amal <ichlas17@gmail.com>
2022-06-04 14:54:40 +0700

    add various images

--- rename.py
+++ rename.py
@@ -2,9 +2,9 @@ from ast import Str
 import os
 import uuid
 
-path = 'U'
+path = 'O'
 
 files = os.listdir(path)
 
 for file in files:
-    os.rename(os.path.join(path,file), 'U.'+str(uuid.uuid1())+'.jpg')
+    os.rename(os.path.join(path,file), 'O.'+str(uuid.uuid1())+'.jpg')
